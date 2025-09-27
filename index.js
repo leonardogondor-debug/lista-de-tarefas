97c0864dc89ff4320d72116632de024558f4466c
leovit <leonardogondor@gmail.com>
2025-09-26 22:56:54 -0300

    ..

---
+++ index.js
@@ -0,0 +1,10 @@
+const atividade = [] 
+atividade.push(document.querySelector("#dados").value);
+
+class Lista {
+    constructor(trabalho,escola,saude) {
+        this.trabalho = trabalho;
+        this.escola = escola;
+        this.saude = saude;
+    }
+}
